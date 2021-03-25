6465ea860a521bbe5bb3b8e2b8878f5c90294c37
wangyuanhang <wangyuanhang@kingsoft.com>
2021-03-25 11:08:18 +0800

    first commit

---
+++ src/main/java/com/hanghang/cloud9/Cloud9Application.java
@@ -0,0 +1,13 @@
+package com.hanghang.cloud9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloud9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanghang/cloud9/Cloud9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghang.cloud9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloud9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
